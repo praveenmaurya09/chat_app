8af3e58623cb740e8df7bf245b632d3ee6914667
praveenmaurya09 <mauryapraveenji@gmail.com>
2023-08-15 07:53:07 +0530

    authScreen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:chat_app/screens/auth.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,13 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 63, 17, 177),),
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
       ),
-      home: ...,
+      home: const AuthScreen(),
     );
   }
 }
